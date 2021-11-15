4ac1b51dfff9401fed5a91f5ffda210eb8dd3209
Каплеева Александра Вячеславовна <kapleeva2@gmail.com>
2021-11-15 12:55:09 +0300

    Выполнила лабораторную работу №2 #7

---
+++ Tasks/Task2/01.sql
@@ -0,0 +1,3 @@
+﻿-- Найти количество разнообразных должностей из таблицы HumanResources.Employee.
+SELECT COUNT(DISTINCT JobTitle) AS Count_Job
+FROM HumanResources.Employee;
---
+++ Tasks/Task2/03.sql
@@ -0,0 +1,6 @@
+﻿-- Показать суммарную стоимость для товара, чья стоимость меньше 100, в каждой линейке размеров Size из таблицы Production.Product (из выборки исключить пустые поля)
+SELECT Size
+	, SUM(ListPrice) AS Sum_Price
+FROM Production.Product
+WHERE ListPrice IS NOT NULL AND ListPrice < 100 AND Size IS NOT NULL
+GROUP BY Size;
