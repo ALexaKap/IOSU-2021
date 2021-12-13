26e5e448a17e956eafd37249e3099244be6e4550
Каплеева Александра Вячеславовна <kapleeva2@gmail.com>
2021-12-13 23:15:31 +0300

    Курсовой проект #15

---
+++ Tasks/Task5/1.sql
@@ -0,0 +1,10 @@
+﻿-- Выдать список дел по ФИО осужденного
+SELECT p.PersonID
+	, p.[Name]
+	, CA.*
+FROM Person AS p 
+INNER JOIN Content AS c
+ON p.PersonID = c.ConvictID
+INNER JOIN [Case] AS CA
+ON CA.ContentID = c.ContentID --; -- Все осужденные, проходящие по всем делам
+WHERE p.[Name] = 'Сидоров Елисей Евгеньевич'; 
---
+++ Tasks/Task5/4.sql
@@ -0,0 +1,13 @@
+﻿-- Выдавать список дел по данной статье (по характеру преступления)
+SELECT q.Qualification
+	, a.Article
+	, CA.*
+FROM Article AS a
+INNER JOIN Qualification AS q
+ON a.ArticleID = q.ArticleID
+INNER JOIN Content AS c
+ON q.QualificationID = c.QualificationID
+INNER JOIN [Case] AS CA
+ON CA.ContentID = c.ContentID --;
+WHERE q.Qualification LIKE '%Средней тяжести%'; 
+--WHERE a.Article LIKE '%ст.105%'; 
