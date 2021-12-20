8889c81ce4805a209b8b3c3ec29270b9218ab057
Каплеева Александра Вячеславовна <kapleeva2@gmail.com>
2021-12-20 15:31:12 +0300

    Лабораторная работа №4 #17

---
+++ Tasks/Task4/4.sql
@@ -0,0 +1,23 @@
+﻿-- Показать товары, цена которых равна минимальной (больше нуля) цене товара того же размера (размер определен) (Таблица Production.Product). Показать поля [Name], ListPrice и Size.
+
+-- Минимальная цена товара для каждого размера
+SELECT MIN(p2.ListPrice)
+	, p2.Size
+FROM Production.Product AS p2
+WHERE p2.ListPrice > 0
+AND p2.Size IS NOT NULL 
+GROUP BY Size
+
+-- Решение
+SELECT p1.[Name]
+	, p1.ListPrice
+	, p1.Size
+FROM Production.Product AS p1
+WHERE p1.Size IS NOT NULL
+AND p1.ListPrice =
+	(
+		SELECT MIN(p2.ListPrice)
+		FROM Production.Product AS p2
+		WHERE p1.Size = p2.Size
+	)
+ORDER BY p1.Size;
---
+++ Tasks/Task4/5.sql
@@ -0,0 +1,24 @@
+﻿-- Показать товары, цена которых больше средней цены в любой линейке продуктов (линейка продуктов определена) (Таблица Production.Product). Показать поля [Name], ListPrice и ProductLine.
+
+-- Средняя цена товаров в каждой линейке продуктов
+SELECT AVG(ListPrice)
+	, ProductLine
+FROM Production.Product
+WHERE ListPrice > 0
+AND ProductLine IS NOT NULL
+GROUP BY ProductLine;
+
+-- Решение (> 965.3488)
+SELECT [Name]
+	, ListPrice
+	, ProductLine
+FROM Production.Product
+WHERE ListPrice > ALL
+	(
+		SELECT AVG(ListPrice)
+		FROM Production.Product
+		WHERE ListPrice > 0
+		AND ProductLine IS NOT NULL
+		GROUP BY ProductLine
+	)
+ORDER BY ListPrice;
